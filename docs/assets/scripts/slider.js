ba7748040c56a90c7315a9abac8fd5525250d16e
Vlad <vladfreishmidt.webdevelopment@gmail.com>
2020-08-21 07:51:16 +0300

    Build task was configured

---
+++ docs/assets/scripts/slider.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function () {
+    $('.slider').slick({
+        nextArrow: '<button class="slick-arrow slick-next"><img src="assets/images/icons/arrow-right.svg" alt="arrow next"></button> ',
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="assets/images/icons/arrow-left.svg" alt="arrow prev"></button> ',
+        dots: true,
+        autoplay: true,
+        speed: 1500,
+        dots: false,
+        pauseOnHover: true,
+    });
+});
+
